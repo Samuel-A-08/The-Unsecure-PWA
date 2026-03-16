56071fbd53080af2f32fa506a8d714f4d49b68c0
Samuel-A-08 <samuel.ablett@education.nsw.gov.au>
2026-03-17 09:51:40 +1100

    Update user_management.py
    fixing sql injections on line 25 and line 45

--- user_management.py
+++ user_management.py
@@ -22,7 +22,7 @@ def retrieveUsers(username, password):
         con.close()
         return False
     else:
-        cur.execute(f"SELECT * FROM users WHERE password = '{password}'")
+        cur.execute("SELECT * FROM users WHERE password = ?", (password,))
         # Plain text log of visitor count as requested by Unsecure PWA management
         with open("visitor_log.txt", "r") as file:
             number = int(file.read().strip())
@@ -42,7 +42,7 @@ def retrieveUsers(username, password):
 def insertFeedback(feedback):
     con = sql.connect("database_files/database.db")
     cur = con.cursor()
-    cur.execute(f"INSERT INTO feedback (feedback) VALUES ('{feedback}')")
+    cur.execute("INSERT INTO feedback (feedback) VALUES (%s)", (feedback,))
     con.commit()
     con.close()
 
