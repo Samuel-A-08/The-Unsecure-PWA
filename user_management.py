8a92dcd97cbf6a0d33c8c415840ac35f95053301
Samuel-A-08 <samuel.ablett@education.nsw.gov.au>
2026-03-17 09:59:08 +1100

    Update user_management.py
    Fully revamped

--- user_management.py
+++ user_management.py
@@ -1,60 +1,57 @@
 import sqlite3 as sql
 import time
 import random
+import html
 
 
 def insertUser(username, password, DoB):
-    con = sql.connect("database_files/database.db")
-    cur = con.cursor()
-    cur.execute(
-        "INSERT INTO users (username,password,dateOfBirth) VALUES (?,?,?)",
-        (username, password, DoB),
-    )
-    con.commit()
-    con.close()
+    with sql.connect("database_files/database.db") as con:
+        cur = con.cursor()
+        cur.execute(
+            "INSERT INTO users (username, password, dateOfBirth) VALUES (?, ?, ?)",
+            (username, password, DoB),
+        )
 
 
 def retrieveUsers(username, password):
-    con = sql.connect("database_files/database.db")
-    cur = con.cursor()
-    cur.execute("SELECT * FROM users WHERE username = ?", (username,))
-    if cur.fetchone() == None:
-        con.close()
+    with sql.connect("database_files/database.db") as con:
+        cur = con.cursor()
+        cur.execute(
+            "SELECT * FROM users WHERE username = ? AND password = ?",
+            (username, password),
+        )
+        user = cur.fetchone()
+
+    if not user:
         return False
-    else:
-        cur.execute("SELECT * FROM users WHERE password = ?", (password,))
-        # Plain text log of visitor count as requested by Unsecure PWA management
-        with open("visitor_log.txt", "r") as file:
+
+    # Update visitor log safely
+    try:
+        with open("visitor_log.txt", "r+") as file:
             number = int(file.read().strip())
-            number += 1
+            file.seek(0)
+            file.write(str(number + 1))
+            file.truncate()
+    except FileNotFoundError:
         with open("visitor_log.txt", "w") as file:
-            file.write(str(number))
-        # Simulate response time of heavy app for testing purposes
-        time.sleep(random.randint(80, 90) / 1000)
-        if cur.fetchone() == None:
-            con.close()
-            return False
-        else:
-            con.close()
-            return True
+            file.write("1")
+
+    time.sleep(random.randint(80, 90) / 1000)
+    return True
 
 
 def insertFeedback(feedback):
-    con = sql.connect("database_files/database.db")
-    cur = con.cursor()
-    cur.execute("INSERT INTO feedback (feedback) VALUES (%s)", (feedback,))
-    con.commit()
-    con.close()
+    with sql.connect("database_files/database.db") as con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO feedback (feedback) VALUES (?)", (feedback,))
 
 
 def listFeedback():
-    con = sql.connect("database_files/database.db")
-    cur = con.cursor()
-    data = cur.execute("SELECT * FROM feedback").fetchall()
-    con.close()
-    f = open("templates/partials/success_feedback.html", "w")
-    for row in data:
-        f.write("<p>\n")
-        f.write(f"{row[1]}\n")
-        f.write("</p>\n")
-    f.close()
+    with sql.connect("database_files/database.db") as con:
+        cur = con.cursor()
+        data = cur.execute("SELECT * FROM feedback").fetchall()
+
+    with open("templates/partials/success_feedback.html", "w") as f:
+        for row in data:
+            safe_text = html.escape(row[1])
+            f.write(f"<p>\n{safe_text}\n</p>\n")
